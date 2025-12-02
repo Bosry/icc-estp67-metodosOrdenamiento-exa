369ec3fcbb6dd4058515728b4ad5e51b6728e4b7
Dayanna <jchachav@est.ups.edu.ec>
2025-12-02 14:19:40 -0500

    Examen Practico

--- src/App.java
+++ src/App.java
@@ -1,11 +1,46 @@
+import controllers.BrandController;
+import models.Brand;
+import test.TestData;
 
 public class App {
-    
+
     public static void main(String[] args) throws Exception {
         System.out.println("Examen interciclo de Estructuras de Datos");
         System.out.println("====Configurar studente.env====");
         System.out.println("Original: ");
+BrandController controller = new BrandController();
 
+        // cargar datos de prueba
+        Brand[] brands = TestData.createBrands();
+
+        System.out.println("=== LISTA ORIGINAL ===");
+        controller.printBrands(brands);
+
+        // ORDENAR SEGÚN FILA A → Selection Sort DESCENDENTE
+        System.out.println("\n=== ORDENADO POR SELECTION SORT DESCENDENTE ===");
+        controller.sortSelectionDesc(brands);
+        controller.printBrands(brands);
+
+        // BÚSQUEDAS SEGÚN LA FILA A
+        System.out.println("\n=== BÚSQUEDA BINARIA (8 años, desc) ===");
+        Brand b1 = controller.binarySearchByValidYears(brands, 8, false);
+        if (b1 != null) {
+            System.out.println("Encontrada: " + b1.getName() +
+                    " - Total de años válidos: " + b1.getTotalValidYears());
+        } else {
+            System.out.println("No encontrada");
+        }
+
+        System.out.println("\n=== BÚSQUEDA BINARIA (10 años, desc) ===");
+        Brand b2 = controller.binarySearchByValidYears(brands, 10, false);
+        if (b2 != null) {
+            System.out.println("Encontrada: " + b2.getName() +
+                    " - Total de años válidos: " + b2.getTotalValidYears());
+        } else {
+            System.out.println("No encontrada");
+        }
+
+        System.out.println("\n=== FIN DE EJECUCIÓN ===");
     }
 
 }
